1ebdbeba8f1dd74cf2dfad49fb9a9325167f45ee
Tina <tina@Tinas-MacBook-Air.local>
2025-01-20 15:09:44 +0800

    feat: update mock data to include 'shareBy' array for each expense item

--- src/mock/mockData.js
+++ src/mock/mockData.js
@@ -3,40 +3,62 @@ export const expenseData = [
     name: "The Chairs",
     color: "#E7D3A7",
     expenses: [
-      { item: "樹屋", amount: 1200 },
-      { item: "RollinOn", amount: 480 },
+      {
+        item: "樹屋",
+        amount: 1200,
+        sharedBy: ["The Chairs", "KANA-BOON", "粗大Band", "老王樂隊"],
+      },
+      { item: "RollinOn", amount: 480, sharedBy: ["The Chairs", "KANA-BOON"] },
     ],
   },
   {
     name: "KANA-BOON",
     color: "#F0B694",
-    expenses: [{ item: "シルエット", amount: 18 }],
+    expenses: [
+      { item: "シルエット", amount: 18, sharedBy: ["KANA-BOON", "溫室雜草"] },
+    ],
   },
   {
     name: "粗大Band",
     color: "#BFAD76",
-    expenses: [{ item: "留下來陪我", amount: 25 }],
+    expenses: [
+      { item: "留下來陪我", amount: 25, sharedBy: ["粗大Band", "DragonPony"] },
+    ],
   },
   {
     name: "DragonPony",
     color: "#C2C2BB",
     expenses: [
-      { item: "Deneb", amount: 66 },
-      { item: "POP UP", amount: 66 },
-      { item: "Morse Code", amount: 66 },
+      { item: "Deneb", amount: 66, sharedBy: ["DragonPony", "老王樂隊"] },
+      { item: "POP UP", amount: 66, sharedBy: ["DragonPony", "溫室雜草"] },
+      {
+        item: "Morse Code",
+        amount: 66,
+        sharedBy: ["DragonPony", "The Chairs"],
+      },
     ],
   },
   {
     name: "老王樂隊",
     color: "#F0B694",
-    expenses: [{ item: "我在愛情的盡頭看見了你和我", amount: 365 }],
+    expenses: [
+      {
+        item: "我在愛情的盡頭看見了你和我",
+        amount: 365,
+        sharedBy: ["老王樂隊", "The Chairs"],
+      },
+    ],
   },
   {
     name: "溫室雜草",
     color: "#C2C2BB",
     expenses: [
-      { item: "在這個年代,找不到浪漫", amount: 2025 },
-      { item: "春天有腳", amount: 30 },
+      {
+        item: "在這個年代,找不到浪漫",
+        amount: 2025,
+        sharedBy: ["溫室雜草", "The Chairs", "老王樂隊", "DragonPony"],
+      },
+      { item: "春天有腳", amount: 30, sharedBy: ["老王樂隊", "粗大Band"] },
     ],
   },
 ];
