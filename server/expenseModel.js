0bb76a8eef0918e843b22d2278392120b242ef57
Tina <tina@Tinas-MacBook-Air.local>
2025-02-08 09:59:41 +0800

    feat: create expense schema and model

---
+++ server/expenseModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const personalExpenseSchema = new mongoose.Schema({
+  item: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [1, "Item name cannot be empty"],
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: [0, "Amount must be greater than 0"],
+  },
+  sharedBy: [{ type: String, required: true }],
+});
+
+const userExpenseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: [1, "User name cannot be empty"],
+  },
+  color: { type: String, required: true },
+  personalExpenses: [personalExpenseSchema],
+});
+
+const expenseSchema = new mongoose.Schema({
+  linkId: { type: String, required: true, unique: true },
+  expenses: [userExpenseSchema],
+});
+
+module.exports = mongoose.model("ExpenseModel", expenseSchema);
