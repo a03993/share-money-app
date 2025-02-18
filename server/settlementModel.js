5eee66942c2b6902cb0e4eabcba0efb5a2327269
Tina <a03993@yahoo.com.tw>
2025-02-18 17:02:33 +0800

    feat: add timestamps

--- server/settlementModel.js
+++ server/settlementModel.js
@@ -11,29 +11,35 @@ const UserInfoSchema = new mongoose.Schema({
   },
 });
 
-const SettlementDetailSchema = new mongoose.Schema({
-  payer: {
-    type: UserInfoSchema,
-    required: true,
+const SettlementDetailSchema = new mongoose.Schema(
+  {
+    payer: {
+      type: UserInfoSchema,
+      required: true,
+    },
+    payee: {
+      type: UserInfoSchema,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "completed"],
+      default: "pending",
+    },
   },
-  payee: {
-    type: UserInfoSchema,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    enum: ["pending", "completed"],
-    default: "pending",
-  },
-});
+  { timestamps: true }
+);
 
-const SettlementSchema = new mongoose.Schema({
-  linkId: { type: String, required: true, ref: "Expense" },
-  settlements: [SettlementDetailSchema],
-});
+const SettlementSchema = new mongoose.Schema(
+  {
+    linkId: { type: String, required: true, ref: "Expense" },
+    settlements: [SettlementDetailSchema],
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Settlement", SettlementSchema);
